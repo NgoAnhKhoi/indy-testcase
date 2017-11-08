d79c70187fa6e3f7b45c6171f405123d7a42cc92
Khoi Ngo <khoi.ngo@logigear.com>
2017-11-08 11:09:26 +0700

    asdf

--- test_scenario_02.py
+++ test_scenario_02.py
@@ -30,13 +30,13 @@ class MyVars:
     """  Needed some global variables. """
 
     pool_handle = 0
-    folder_path = "~/.sovrin/" #os.path.expanduser("~") + "/.sovrin/"
+    folder_path = os.path.expanduser("~") + "/.sovrin/"
     pool_genesis_txn_file = "pool_transactions_sandbox_genesis"
     original_pool_genesis_txn_file = "original_pool_transactions_sandbox_genesis"
 
     pool_genesis_txn_file_path = folder_path + pool_genesis_txn_file
     original_pool_genesis_txn_file_path = folder_path + original_pool_genesis_txn_file
-    pool_name = "t_pl123l"
+    pool_name = "test_" + str(random.randrange(100, 1000, 2))
     debug = False
     the_error_message = "the information needed to connect was not found"
     test_results = {'Test 3': False}
@@ -47,68 +47,44 @@ logging.basicConfig(level=logging.INFO)
 
 
 def command(command_str):
-#     print("in command")
     os.system(command_str)
-#     stdout = ""
-#     try:
-#         process = Popen(['sovrin'], stdin = PIPE, stdout = PIPE)
-#         subprocess.call(['connect test'], stdin = PIPE, stdout = PIPE)
-#         subprocess.call(['exit'], stdin = PIPE, stdout = PIPE)
-# #         while True:
-# #             if 'connecting test' in str(process.stdout.readline()):
-# #                 
-# #                 break
-# #         process.terminate()
-#     except TimeoutExpired:
-#         stdout = "asdf"
-# #     subprocess.call(command_str)
-# #     subprocess.run(command_str, time_out=30)
-# #     stdout = check_output(command_str, stderr=STDOUT, timeout=30)
-# #     stdout = "" #"p.communicate()[0]
-# #     try:
-# #         stdout = process.communicate(timeout=10)[0]
-# #     except TimeoutExpired:
-# #         os.kill(process.pid, signal.SIGINT) # send signal to the process group
-# #         stdout = process.communicate()[0]
-# #     print("out command")
-#     return stdout
 
 def test_precondition():
     """  Make a copy of pool_transactions_sandbox_genesis  """
     print(Colors.HEADER + "\n\ Precondition \n" + Colors.ENDC)
-#     command(['cd', '~/.sovrin'])
-    command(['cp', MyVars.pool_genesis_txn_file_path, MyVars.original_pool_genesis_txn_file_path])
-#     open(MyVars.pool_genesis_txn_file_path, 'w').close()
+    command('cp ' + MyVars.pool_genesis_txn_file_path + " " + MyVars.original_pool_genesis_txn_file_path)
+    open(MyVars.pool_genesis_txn_file_path, 'w').close()
 
 
 async def verifying_the_correct_message_is_shown_when_you_are_unable_to_connect_to_the_validator_pool():
     logger.info("Test Scenario 02 -> started")
 
-    # 1. Using sovrin command -----------------------------
-    print(Colors.HEADER + "\n\t1. using sovrin\n" + Colors.ENDC)
+    # 1. Create ledger config from genesis txn file  ---------------------------------------------------------
+    print(Colors.HEADER + "\n\t1.  Create Ledger\n" + Colors.ENDC)
+    pool_config = json.dumps({"genesis_txn": str(MyVars.pool_genesis_txn_file_path)})
     try:
-        return_message = await command(['sovrin', 'connect test'])
+        await pool.create_pool_ledger_config(MyVars.pool_name, pool_config)
     except IndyError as E:
         print(Colors.FAIL + str(E) + Colors.ENDC)
+        sys.exit[1]
 
-    # 2. connect test with the empty pool_transactions_sandbox_genesis file --------------------------
-#     print(Colors.HEADER + "\n\t2.  Create Ledger\n" + Colors.ENDC)
-#     pool_config = json.dumps({"genesis_txn": str(MyVars.pool_genesis_txn_file_path)})
-#     try:
-#         await pool.create_pool_ledger_config(MyVars.pool_name, pool_config)
-#     except IndyError as E:
-#         print(Colors.FAIL + str(E) + Colors.ENDC)
-#         sys.exit[1]
-# 
-#     await asyncio.sleep(1)
+    await asyncio.sleep(0)
+
+    # 2. Open pool ledger -----------------------------------------------------------------------------------
+    print(Colors.HEADER + "\n\t2.  Open pool ledger\n" + Colors.ENDC)
+    try:
+        result = await pool.open_pool_ledger(MyVars.pool_name, None)
+    except IndyError as E:
+        print(Colors.FAIL + str(E) + Colors.ENDC)
+        sys.exit(1)
 
     # 3. verifying the message ------------------------------------------------------------------------
     print(Colors.HEADER + "\n\t3. verifying the message\n" + Colors.ENDC)
     try:
         print("error_message: " + MyVars.the_error_message)
 #         return_message = get_output()
-        print("output_message: " + return_message)
-        if (return_message != MyVars.the_error_message):
+        print("output_message: " + str(result))
+        if (str(result) != MyVars.the_error_message):
             MyVars.test_results['Test 3'] = True
     except IndyError as E:
         print(Colors.FAIL + str(E) + Colors.ENDC)
@@ -126,11 +102,12 @@ async def verifying_the_correct_message_is_shown_when_you_are_unable_to_connect_
     #      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! End of test, run cleanup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # ==================================================================================================================
     # 5. Restore the pool_transactions_sandbox_genesis file ------------------------------------------------------------------------------
-#     print(Colors.HEADER + "\n\t==Clean up==\n\t5. Restore the pool_transactions_sandbox_genesis file\n" + Colors.ENDC)
-#     try:
-#         await command(['rm', 'pool_transactions_sandbox_genesis', 'mv', 'original_pool_trasnsactions_sandbox_genesis', 'pool_transactions_sandbox_genesis'])
-#     except IndyError as E:
-#         print(Colors.FAIL + str(E) + Colors.ENDC)
+    print(Colors.HEADER + "\n\t==Clean up==\n\t5. Restore the pool_transactions_sandbox_genesis file\n" + Colors.ENDC)
+    try:
+        command('rm ' + MyVars.pool_genesis_txn_file_path)
+        command('mv ' + MyVars.pool_genesis_txn_file_path + " " + MyVars.original_pool_genesis_txn_file_path)
+    except IndyError as E:
+        print(Colors.FAIL + str(E) + Colors.ENDC)
 
     logger.info("Test Scenario 02 -> completed")
 
@@ -162,34 +139,13 @@ async def test_connect():
 
     print("end")
 
+# test_precondition()
+test_precondition()
 # Create the loop instance using asyncio
 loop = asyncio.get_event_loop()
-loop.run_until_complete(test_connect())
+loop.run_until_complete(verifying_the_correct_message_is_shown_when_you_are_unable_to_connect_to_the_validator_pool())
 loop.close()
 
 
-
-
-
-#     finally:
-#         cmd = 'cp ' + MyVars.folder_path + "khoi_pool " + MyVars.pool_genesis_txn_file_path
-#         os.system('cmd')
-#     subprocess.call(['connect test'])
-# command(test1)
-# command(test1)
-
-# async def abc():
-#     test = ['sovrin','connect test']
-#     test1 = ["exit"]
-#     await command(test)
-#     await command(test1)
-# test_precondition()
-
-# Create the loop instance using asyncio
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(khoi())
-# loop.close()
-
-print("done")
-# print("\n\nResults\n+" + 40*"=" + "+")
-# final_results()
+print("\n\nResults\n+" + 40*"=" + "+")
+final_results()
