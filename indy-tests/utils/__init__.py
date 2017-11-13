1271952a76d7f152ba50f2f231150c1de12d72df
Khoi Ngo <khoi.ngo@logigear.com>
2017-11-13 13:41:24 +0700

    asdadf

---
+++ indy-tests/utils/__init__.py
@@ -0,0 +1,2 @@
+import os, sys
+__path__ = sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
--- indy-tests/utils/utils.py
+++ indy-tests/utils/utils.py
@@ -23,3 +23,24 @@ def generate_random_string(prefix="", suffix="", size=20):
     print("Warning: Length of prefix and suffix more than %s chars", str(size))
     result = str(prefix) + random_str + str(suffix)
     return result
+
+
+def clean_up_pool_and_wallet(pool_name="", wallet_name=""):
+    import os
+    import shutil
+    from .constant import Colors
+    print(Colors.HEADER + "\n\tCheck if the wallet and pool for this test already exist and delete them...\n" + Colors.ENDC)
+    x = os.path.expanduser('~')
+    work_dir = x + os.sep + ".indy"
+
+    if os.path.exists(work_dir + "/pool/" + pool_name):
+        try:
+            shutil.rmtree(work_dir + "/pool/" + pool_name)
+        except IOError as E:
+            print(Colors.FAIL + str(E) + Colors.ENDC)
+
+    if os.path.exists(work_dir + "/wallet/" + wallet_name):
+        try:
+            shutil.rmtree(work_dir + "/wallet/" + wallet_name)
+        except IOError as E:
+            print(Colors.FAIL + str(E) + Colors.ENDC)
