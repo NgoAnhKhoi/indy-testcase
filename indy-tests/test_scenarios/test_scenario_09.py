aae25b7dd591f29ff8bf84ac5c963a2aa8f6c232
Nhan Nguyen - 6187 <nhan.trong.nguyen@logigear.com>
2017-11-15 13:54:35 +0700

    commit

--- indy-tests/test_scenarios/test_scenario_09.py
+++ indy-tests/test_scenarios/test_scenario_09.py
@@ -82,10 +82,10 @@ def check_role_in_retrieved_nym(retrieved_nym, role):
         return False
     print(Colors.HEADER + "\n" + retrieved_nym + "\n" + Colors.ENDC)
     nym_dict = json.loads(retrieved_nym)
-    if "data" in nym_dict:
+    if True:
         temp_dict = json.loads(nym_dict["data"].decode('string_escape'))
-        print(Colors.HEADER + "\n" + "AAAAA" + "\n" + Colors.ENDC)
-        if "role" in temp_dict:
+        print(Colors.HEADER + "\n" + temp_dict + "\n" + Colors.ENDC)
+        if True:
             if temp_dict["role"] == role:
                 return False
             else:
